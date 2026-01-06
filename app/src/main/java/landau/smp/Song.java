4e398c27499959f7c5ec5884254ce8093545ce76
Alex Landau <landau.alex@gmail.com>
2026-01-06 14:20:03 -0800

    Improve ID3 charset detection

--- app/src/main/java/landau/smp/Song.java
+++ app/src/main/java/landau/smp/Song.java
@@ -8,18 +8,26 @@ import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CharsetEncoder;
+import java.nio.charset.StandardCharsets;
 
 @SuppressWarnings("WeakerAccess")
 public class Song {
     private static final String TAG = Song.class.getSimpleName();
 
     private boolean extracted = false;
-    private String filename;
+    private final String filename;
     private String artist, album, title;
     private int durationMs;
 
-    private static CharsetEncoder encoder = Charset.forName("windows-1252").newEncoder();
-    private static CharsetDecoder decoder = Charset.forName("windows-1251").newDecoder();
+    private static final CharsetEncoder[] encoders = {
+            Charset.forName("windows-1252").newEncoder(),
+            Charset.forName("EUC-JP").newEncoder(),
+            Charset.forName("GBK").newEncoder(),
+            Charset.forName("Shift_JIS").newEncoder(),
+    };
+    private static final CharsetDecoder decoder = Charset.forName("windows-1251").newDecoder();
+    private static final CharsetDecoder utf8decoder = StandardCharsets.UTF_8.newDecoder();
+    private static final CharsetEncoder win1251encoder = Charset.forName("windows-1251").newEncoder();
 
     public Song(String filename) {
         this.filename = filename;
@@ -45,7 +53,8 @@ public class Song {
         album = getTagWithFallback(retriever, MediaMetadataRetriever.METADATA_KEY_ALBUM, 25);
         title = getTagWithFallback(retriever, MediaMetadataRetriever.METADATA_KEY_TITLE, 31);
         try {
-            durationMs = Integer.valueOf(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
+            //noinspection DataFlowIssue (extractMetadata may return null)
+            durationMs = Integer.parseInt(retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION));
         } catch (NumberFormatException e) {
             durationMs = 0;
         }
@@ -61,6 +70,16 @@ public class Song {
     public String getTitle() { return title; }
     public int getDuration() { return durationMs; }
 
+    private boolean isNameGood(String s) {
+        int badChars = 0;
+        for (int i = 0; i < s.length(); i = s.offsetByCodePoints(i, 1)) {
+            int c = s.codePointAt(i);
+            if (c >= 0x0600 && Character.getType(c) != Character.OTHER_SYMBOL) {
+                badChars++;
+            }
+        }
+        return badChars <= 1;
+    }
     private String getTagWithFallback(MediaMetadataRetriever retriever, int key1, int key2) {
         String s = retriever.extractMetadata(key1);
         if (s == null) {
@@ -73,10 +92,39 @@ public class Song {
         if (s.isEmpty()) {
             return null;
         }
+
+        /*
+        for (Map.Entry<String,Charset> d: Charset.availableCharsets().entrySet()) {
+            for (Map.Entry<String,Charset> e: Charset.availableCharsets().entrySet()) {
+//                CharBuffer res = Charset.forName("windows-1251").decode(e.getValue().encode(CharBuffer.wrap(s)));
+                CharBuffer res = d.getValue().decode(e.getValue().encode(CharBuffer.wrap(s)));
+                if (res.length() != 0 && res.get(0) >= 'А' && res.get(0) <= 'Я')
+                    Log.i(TAG, d.getKey() + ","+e.getKey() + ": " + res);
+            }
+        }
+        */
+
         try {
-            return decoder.decode(encoder.encode(CharBuffer.wrap(s))).toString();
+            String fixed = utf8decoder.decode(win1251encoder.encode(CharBuffer.wrap(s))).toString();
+            if (isNameGood(fixed)) {
+                return fixed;
+            }
         } catch (CharacterCodingException e) {
+            // continue
+        }
+        if (isNameGood(s)) {
             return s;
         }
+        for (CharsetEncoder e : encoders) {
+            try {
+                String fixedName = decoder.decode(e.encode(CharBuffer.wrap(s))).toString();
+                if (isNameGood(fixedName)) {
+                    return fixedName;
+                }
+            } catch (CharacterCodingException ex) {
+                // continue loop
+            }
+        }
+        return null;
     }
 }
