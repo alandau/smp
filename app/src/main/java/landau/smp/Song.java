a5536c8eacda884d9d40fce1652099f05ae8c3f8
Alex Landau <landau.alex@gmail.com>
2026-01-12 16:21:16 -0800

    Remove pre-ID3 charset detection code

--- app/src/main/java/landau/smp/Song.java
+++ app/src/main/java/landau/smp/Song.java
@@ -2,16 +2,12 @@ package landau.smp;
 
 import android.media.MediaMetadataRetriever;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
 import java.nio.charset.CharacterCodingException;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
 import java.nio.charset.CharsetEncoder;
-import java.nio.charset.StandardCharsets;
 
 @SuppressWarnings("WeakerAccess")
 public class Song {
@@ -22,17 +18,8 @@ public class Song {
     private String artist, album, title;
     private int durationMs;
 
-    private static final CharsetEncoder[] encoders = {
-            Charset.forName("windows-1252").newEncoder(),
-            Charset.forName("windows-1253").newEncoder(),
-            Charset.forName("windows-1256").newEncoder(),
-            Charset.forName("EUC-JP").newEncoder(),
-            Charset.forName("GBK").newEncoder(),
-            Charset.forName("Shift_JIS").newEncoder(),
-    };
+    private static final CharsetEncoder encoder = Charset.forName("windows-1252").newEncoder();
     private static final CharsetDecoder decoder = Charset.forName("windows-1251").newDecoder();
-    private static final CharsetDecoder utf8decoder = StandardCharsets.UTF_8.newDecoder();
-    private static final CharsetEncoder win1251encoder = Charset.forName("windows-1251").newEncoder();
 
     public Song(String filename) {
         this.filename = filename;
@@ -86,44 +73,6 @@ public class Song {
     public String getTitle() { return title; }
     public int getDuration() { return durationMs; }
 
-    private boolean isMaybeKoi8(String s) {
-        boolean seenNonRuLetter = false;
-        int state = 0; // 0 - non-letter, 1 - capital, 2 - small
-        for (char c : s.toCharArray()) {
-            switch (state) {
-                case 0:
-                    if (c >= 'А' && c <= 'Я') state = 1; // capital at word start -> maybe koi (e.g. second word)
-                    else if (c >= 'а' && c <= 'я') state = 2;
-                    // else, non-letter -> same state
-                    break;
-                case 1:
-                    if (c >= 'а' && c <= 'я') return false; // small after capital -> not koi
-                    //noinspection StatementWithEmptyBody
-                    if (c >= 'А' && c <= 'Я') { /* do nothing */} // capital after capital -> maybe koi
-                    else state = 0; // non-letter after capital
-                    break;
-                case 2:
-                    if (c >= 'а' && c <= 'я') return false; // small after small -> not koi
-                    if (c >= 'А' && c <= 'Я') state = 1; // capital after small -> maybe koi
-                    else state = 0; // non-letter after small -> maybe koi
-                    break;
-            }
-            if (state != 0) {
-                seenNonRuLetter = true;
-            }
-        }
-        return seenNonRuLetter;
-    }
-    private boolean isNameGood(String s) {
-        int badChars = 0;
-        for (int i = 0; i < s.length(); i = s.offsetByCodePoints(i, 1)) {
-            int c = s.codePointAt(i);
-            if (c >= 0x0600 && Character.getType(c) != Character.OTHER_SYMBOL) {
-                badChars++;
-            }
-        }
-        return badChars <= 1;
-    }
     private String getTagWithFallback(MediaMetadataRetriever retriever, int key1, int key2) {
         String s = retriever.extractMetadata(key1);
         if (s == null) {
@@ -137,49 +86,10 @@ public class Song {
             return null;
         }
 
-        /*
-        for (java.util.Map.Entry<String,Charset> d: Charset.availableCharsets().entrySet()) {
-            for (java.util.Map.Entry<String,Charset> e: Charset.availableCharsets().entrySet()) {
-//                CharBuffer res = Charset.forName("windows-1251").decode(e.getValue().encode(CharBuffer.wrap(s)));
-                CharBuffer res = d.getValue().decode(e.getValue().encode(CharBuffer.wrap(s)));
-                if (res.length() != 0 && res.get(0) >= 'А' && res.get(0) <= 'Я')
-                    android.util.Log.i(TAG, d.getKey() + ","+e.getKey() + ": " + res);
-            }
-        }
-        */
-
         try {
-            String fixed = utf8decoder.decode(win1251encoder.encode(CharBuffer.wrap(s))).toString();
-            if (isNameGood(fixed)) {
-                return fixed;
-            }
+            return decoder.decode(encoder.encode(CharBuffer.wrap(s))).toString();
         } catch (CharacterCodingException e) {
-            // continue
-        }
-
-        if (isNameGood(s) && isMaybeKoi8(s)) {
-            try {
-                String fixed = decoder.decode(Charset.forName("KOI8-R").encode(CharBuffer.wrap(s))).toString();
-                if (isNameGood(fixed)) {
-                    return fixed;
-                }
-            } catch (CharacterCodingException e) {
-                // continue
-            }
-        }
-        if (isNameGood(s)) {
             return s;
         }
-        for (CharsetEncoder e : encoders) {
-            try {
-                String fixedName = decoder.decode(e.encode(CharBuffer.wrap(s))).toString();
-                if (isNameGood(fixedName)) {
-                    return fixedName;
-                }
-            } catch (CharacterCodingException ex) {
-                // continue loop
-            }
-        }
-        return null;
     }
 }
