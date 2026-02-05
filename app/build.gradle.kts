d6cd55562b7e623577833c701a48b72c7bad1879
Alex Landau <landau.alex@gmail.com>
2026-02-04 16:42:42 -0800

    Remove kotlin stdlib from apk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -38,14 +36,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-    buildFeatures {
-        compose = true
-    }
 }
 
 dependencies {
-    implementation(libs.androidx.annotation.jvm)
+    // 1.3 seems to be the last version that doesn't pull in the kotlin stdlib
+    implementation("androidx.annotation:annotation:1.3.0")
 }
